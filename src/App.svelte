cab8504381cd81e86d7213752bca53f6b75886ea
dion.jones <monkey@deadcatdesigns.co.uk>
2021-05-03 20:43:50 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let name;
+	function toggleName(){
+		name = name==='world'?'jones':'world';
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<button on:click={toggleName}>Toggle Name</button>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
